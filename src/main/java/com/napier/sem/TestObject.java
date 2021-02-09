ae3a11e16ab13eec4c13d1db461cdf63dd736d4c
40343450 <54434644+40343450@users.noreply.github.com>
2021-02-09 21:16:48 +0000

    adding pojo for exmaple

---
+++ src/main/java/com/napier/sem/TestObject.java
@@ -0,0 +1,31 @@
+package com.napier.sem;
+
+public class TestObject {
+
+    private String firstName;
+    private String secondName;
+
+    public TestObject() {
+    }
+
+    public TestObject(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+}
